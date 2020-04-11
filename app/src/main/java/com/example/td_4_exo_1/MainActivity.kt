f6ca7bc012eec2217fd7a0eed8e2ab03d61876c4
Your Name <you@example.com>
2020-04-11 04:15:47 +0100

    Exo 1 TD4

---
+++ app/src/main/java/com/example/td_4_exo_1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.td_4_exo_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
